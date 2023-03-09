d7e8e1a834700cab3238afd7c0a9c1947c36136f
ZEAL07 <125942199+ZEAL07@users.noreply.github.com>
2023-03-09 21:12:08 +0800

    Update config.h

--- keyboard/gt-ble60/f/config.h
+++ keyboard/gt-ble60/f/config.h
@@ -40,11 +40,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // 键盘省电参数
 #define SLEEP_SLOW_TIMEOUT 15 // 键盘闲置多久后转入慢速扫描模式 (s)
-#define SLEEP_OFF_TIMEOUT 1200 // 键盘闲置多久后转入自动关机 (s)
+#define SLEEP_OFF_TIMEOUT 600 // 键盘闲置多久后转入自动关机 (s)
 #define KEYBOARD_SCAN_INTERVAL 1 // 键盘最小时间单位TICK (ms)
 #define KEYBOARD_FAST_SCAN_INTERVAL 10 // 通常模式下，多久扫描一次键盘 (ms)
 #define KEYBOARD_SLOW_SCAN_INTERVAL 100 // 慢速模式下，多久扫描一次键盘 (ms)
 #define LED_AUTOOFF_TIME 5 /* LED自动熄灭时长(s)，设为0则不自动熄灭 */
+#define DCDC_ENABLED // 启用DC-DC以降低功耗
 
 // 键盘额外功能
 //#define DYNAMIC_TX_POWER /* 启用自动发射功率调整 */
@@ -58,48 +59,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CONFIG_STORAGE /* 启用配置存储功能 */
 
 // #define DEBUG_SKIP_PWRON_CHECK /*  直接开机而跳过开机条件检测，用于调试 */
+#define BUTTONLESS_DFU
 
-/* TMK固件内置功能 */
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
-
-// LED 配置
-// #define LED_CAPS 4  //caps led ：5 ； led1、2、3、4：19、20、27、26
-
-// ws2812 RGB 配置
-#define RGB_DI_PIN 1
-#define RGBLED_NUM 8
-#define RGBLIGHT_ANIMATIONS
-#define RGB_PWR_PIN 0 // P-mos
-//#define RGB_PWR_PIN_REVERSE 12 // N-mos
-
-// 3灯指示配置引脚
-// #define LED_STATUS_BLE 19
-// #define LED_STATUS_CHARGING 18
-// #define LED_STATUS_USB 20
-// #define LED_BLE_CHANNEL1 19
-// #define LED_BLE_CHANNEL2 20
-// #define LED_BLE_CHANNEL3 18
-#define LED_POSITIVE // LED上拉驱动
-
-// RGB 配置
-#define LED_RGB_CC // 是否为共阴LED
-#define LED_RGB_R 5
-#define LED_RGB_G 4
-#define LED_RGB_B 3
-#define RGB_LIGHT_ANIMATIONS
-
-// 独立硬件按钮
-#define POWER_BUTTON 21
+#define LED_CAPS 19
+#define LED_RGB_R 24
+#define LED_RGB_G 23
+#define LED_RGB_B 22
+#define LED_STATUS_BLE 22
+#define LED_STATUS_CHARGING 23
+#define LED_STATUS_USB 24
 
 // USB UART 传输配置
 #define HAS_USB // 启用与CH554的通信支持
-#define UART_RXD 6 // UART_RX口IO 17
-#define UART_TXD 7 // UART_TX口IO 18
+#define UART_RXD 17 // UART_RX口IO 17
+#define UART_TXD 18 // UART_TX口IO 18
 //#define UART_DET 19 // UART 检测引脚，若此脚被拉低，则说明USB正在工作。若不配置则使用RX口作为检测引脚
 #define UART_BAUDRATE NRF_UART_BAUDRATE_115200 // 通信波特率，请不要修改
 
@@ -110,12 +83,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define PIN_CHARGING !UCC1
 //#define PIN_STANDBY !UCC2
 
-// 多用途 Bootloader 按钮
-#define NRF_BL_DFU_MULTI_ROLE_BTN 21
-// Bootloader指示灯
-#define LED_DFU_INIT 5
-#define LED_DFU_START 4
-#define LED_DFU_FINISH 3
 
 // 按键阵列配置
 #define MATRIX_ROWS 5 /* 硬件阵列行数 */
@@ -125,23 +92,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
 
-static const uint8_t row_pin_array[MATRIX_ROWS] = {31, 30, 29, 28, 27};
-static const uint8_t column_pin_array[MATRIX_COLS] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 25, 26, 10};
+#define PIN_CHARGING !P3_3
+#define PIN_STANDBY !P3_4
+
+
+static const uint8_t row_pin_array[MATRIX_ROWS] = { 25, 28, 29, 30, 26 };
+static const uint8_t column_pin_array[MATRIX_COLS] = { 16, 15, 14, 13, 12, 11, 27, 3, 4, 5, 6, 7, 8, 9 };
 
 #define ROW_IN // 键盘阵列的二极管方向是从COL->ROW
 
-// 旋钮配置
-#define ROTARY_ENCODER_A 9
-#define ROTARY_ENCODER_B 8
-// 旋钮正向按钮映射
-#define ROTARY_ENCODER_POS 4,3
-// 旋钮负向按钮映射
-#define ROTARY_ENCODER_NEG 4,4
-// 独立旋钮按键  用于关机后开机
-#define ROTARY_BUTTON 10
-// SSD1306 LCD屏幕
-#define SSD1306_SDA 23
-#define SSD1306_SCL 24
 
 /* define if matrix has ghost */
 // #define MATRIX_HAS_GHOST /* 按键阵列是否出现Ghost Key，若没有加二极管则需要启用这个项目 */
@@ -149,14 +108,3 @@ static const uint8_t column_pin_array[MATRIX_COLS] = {11, 12, 13, 14, 15, 16, 17
 #define DEBOUNCE 5 /* 硬件消抖次数，设置为0则不消抖 */
 #define MATRIX_SCAN_DELAY_CYCLE 48 /* 按键扫描等待IO稳定的延时时长 */
 
-// 采用内部RC
-#define NRFX_CLOCK_CONFIG_LF_SRC 0
-#define CLOCK_CONFIG_LF_SRC 0
-#define NRF_SDH_CLOCK_LF_SRC 0
-#define NRF_SDH_CLOCK_LF_RC_CTIV 16
-#define NRF_SDH_CLOCK_LF_RC_TEMP_CTIV 2
-#define NRF_SDH_CLOCK_LF_ACCURACY 1
-
-//配置RTC预分频器.
-// <0> 32MHz <1> 16MHz <3> 8MHz <7> 4MHz <15> 2MHz <31> 1MHz 
-#define APP_TIMER_CONFIG_RTC_FREQUENCY 0
